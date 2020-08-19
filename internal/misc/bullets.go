e7bb467a4fa1fbdecfb0b6ffdd5aecff5d05f23a
Writhe <filip.moroz@gmail.com>
2020-08-19 20:19:00 +0200

    chore: fixed comment

--- internal/misc/bullets.go
+++ internal/misc/bullets.go
@@ -4,7 +4,7 @@ import "math/rand"
 
 var bullets = []string{":green_heart:", ":heart:", ":blue_heart:", ":yellow_heart:", ":purple_heart:", ":orange_heart:", ":black_heart:"}
 
-// RandomBullet - returns a random exclamation
+// RandomBullet - returns a random bullet
 func RandomBullet() string {
 	return bullets[rand.Intn(len(bullets))]
 }
